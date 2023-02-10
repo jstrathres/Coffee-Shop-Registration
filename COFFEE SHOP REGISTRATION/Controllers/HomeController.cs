0d3a6d1cade75feb8e6b2812277bb2f159efda45
jstrathres <jstraz7@gmail.com>
2023-02-09 20:04:50 -0500

    fist

---
+++ COFFEE SHOP REGISTRATION/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using COFFEE_SHOP_REGISTRATION.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace COFFEE_SHOP_REGISTRATION.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Form()
+        {
+            return View();
+        }
+
+        public IActionResult Result(Coffee c)
+        {
+            return View(c);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ COFFEE SHOP REGISTRATION/Models/Coffee.cs
@@ -0,0 +1,11 @@
+﻿namespace COFFEE_SHOP_REGISTRATION.Models
+{
+    public class Coffee
+    {
+        public string name { get; set; }
+     
+        public string email { get; set; }
+
+        public string password { get; set; }
+    }
+}
